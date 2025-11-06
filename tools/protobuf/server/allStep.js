0a1a01c403fbdacce2ba725fc9fd948a816190a0
积木 <790325011@qq.com>
2025-11-06 17:36:23 +0800

    优化

--- tools/protobuf/server/allStep.js
+++ tools/protobuf/server/allStep.js
@@ -3,10 +3,15 @@ let fs = require('fs');
 
 try {
     console.log('准备执行编译命令...');
-   // process.execSync('tsc');
+    // 判断dist目录是否存在
+    if (!fs.existsSync('./dist')) {
+        process.execSync('tsc');
+    }
 } catch (error) {
-    console.error('请检查是否安装tsc模块,如未安装请执行: npm install -g typescript');
-    return;
+    if (!fs.existsSync('./dist')) {
+        console.error('请检查是否安装tsc模块,如未安装请执行: npm install -g typescript\n', error);
+        return;
+    }
 }
 console.log('开始生成任务...');
 process.execSync('node ./dist/src/main.js');
